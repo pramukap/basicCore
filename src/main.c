dbcefd145157ffc94d8c1f213ae156054f717af0
pramukap <pramuka.perera@gmail.com>
2021-12-15 23:30:55 -0600

    Made context print more reader-friendly

--- src/main.c
+++ src/main.c
@@ -5,6 +5,23 @@ uint8_t mem[MEM_SIZE] = {0};
 
 core_state basic_core;
 
+void print_context(core_state core, int curr_step) {
+        printf("Cycle %d:\n", curr_step);
+        
+        printf("\tIP: 0x%08X ", core.ip);
+        printf("IR: 0x%08X ", core.ir);
+        printf("LR: 0x%08X ", core.lr);
+        printf("MSR: 0x%08X\n\t", core.msr);
+
+        for (int i = 0; i < GPR_FILE_SIZE; i++) {
+            printf("GPR %02i: 0x%08X ", i, core.gpr_file[i]);
+            if (i % 4 == 3) {
+                printf("\n\t");
+            }
+        }
+        printf("-----\n");
+}
+
 int main() {
     // init
     basic_core.ip = 0x0;
@@ -21,10 +38,7 @@ int main() {
     // insert program here
 
     printf("Initial state:\n");
-    for (int i = 0; i < GPR_FILE_SIZE; i++) {
-        printf("GPR %i: 0x%08X\n", i, basic_core.gpr_file[i]);
-    }
-    printf("-----\n");
+    print_context(basic_core, -1);
 
     // run core
     int num_steps = 20;
@@ -33,15 +47,7 @@ int main() {
         basic_core.ir = core_fetch(&basic_core.ip, mem);
         core_decode_execute_complete(&basic_core, mem); 
 
-        printf("Step %d:\n", i + 1);
-        printf("IR: 0x%08X\n", basic_core.ir);
-        printf("IP: 0x%08X\n", basic_core.ip);
-        printf("LR: 0x%08X\n", basic_core.lr);
-        printf("MSR: 0x%08X\n", basic_core.msr);
-        for (int i = 0; i < GPR_FILE_SIZE; i++) {
-            printf("GPR %i: 0x%08X\n", i, basic_core.gpr_file[i]);
-        }
-        printf("-----\n");
+        print_context(basic_core, i);
 
         i++;
     }    
