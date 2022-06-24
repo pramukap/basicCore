5429cc794f4bd2a14c13308971ce6ca15fe3f15a
pramukap <pramuka.perera@gmail.com>
2022-06-23 22:55:26 -0500

    Added comments to describe context registers

--- src/basic_core.h
+++ src/basic_core.h
@@ -4,13 +4,13 @@
 #include "common.h"
 
 typedef struct core_state_type {
-    uint32_t ip;
-    uint32_t ir;
-    uint32_t lr;
-    uint32_t usp;
-    uint32_t psp;
-    uint32_t msr;
-    uint32_t pidr;
+    uint32_t ip;    // instruction pointer
+    uint32_t ir;    // instruction register
+    uint32_t lr;    // link register
+    uint32_t usp;   // user-mode stack pointer
+    uint32_t psp;   // priv-mode stack pointer
+    uint32_t msr;   // machine state register
+    uint32_t pidr;  // process id register
     uint32_t gpr_file[GPR_FILE_SIZE];
 } core_state;
 
